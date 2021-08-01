7bd433f3de26639575dcdbfd63d334db40a75479
victoria2012 <victoria2012@naver.com>
2021-08-01 14:59:13 +0900

    Database lesson, 20210730 수업내용

--- scraping/daum_economic_scrapping.py
+++ scraping/daum_economic_scrapping.py
@@ -10,13 +10,14 @@ if res.status_code == 200:
     # with sqlite3.connect('./db.sqlite3') as conn
     connect = sqlite3.connect('../db.sqlite3')
     cursor = connect.cursor()
+    # cursor.execute("create table daum_news(create_date, href, title)")  #이렇게 table을 만들 수도 있다
 
     for link in links:
         title = str.strip(link.get_text())
         href = str.strip(link.get('href'))
         try:
             cursor.execute(
-                "insert into polls_economics(create_date, href, title) values(datetime('now'), ?, ?)", (href,title))
+                "insert into daum_news(create_date, href, title) values(datetime('now'), ?, ?)", (href,title))
             print(title, ' : ', href)
 
         except:
